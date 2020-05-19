dd1fb293f3cf5863790ce365862c8694681ea619
ndickson <3254194+ndickson@users.noreply.github.com>
2020-05-19 00:21:53 -0400

    Added initial ImageButton.h

---
+++ include/ImageButton.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "UIBox.h"
+#include "UICommon.h"
+
+#include <Types.h>
+
+OUTER_NAMESPACE_BEGIN
+UICOMMON_LIBRARY_NAMESPACE_BEGIN
+
+struct Image {
+	Array<Vec4f> pixels;
+	size_t width;
+	size_t height;
+
+	void clear() {
+		pixels.setCapacity(0);
+		width = 0;
+		height = 0;
+	}
+};
+
+struct ImageButton : public UIBox {
+	// This is the image if !isDisabled && !isMouseInside && !isMouseDown.
+	Image upImage;
+
+	// This is the image if !isDisabled && (isMouseInside != isMouseDown).
+	Image hoverImage;
+
+	// This is the image if !isDisabled && isMouseInside && isMouseDown.
+	Image downImage;
+
+	// This is the image if isDisabled.
+	Image disabledImage;
+
+	void (*actionCallback)(ImageButton&);
+	void* callbackData;
+
+	bool isMouseInside;
+	bool isMouseDown;
+	bool isDisabled;
+
+
+	UICOMMON_LIBRARY_EXPORTED static const UIBoxClass staticType;
+
+	UICOMMON_LIBRARY_EXPORTED ImageButton(
+		const char* upImageFilename,
+		const char* hoverImageFilename,
+		const char* downImageFilename,
+		const char* disabledImageFilename = nullptr
+	);
+
+protected:
+	UICOMMON_LIBRARY_EXPORTED static UIBox* construct();
+	UICOMMON_LIBRARY_EXPORTED static void destruct(UIBox* box);
+	UICOMMON_LIBRARY_EXPORTED static void onMouseDown(UIBox& box, size_t button, const MouseState& state);
+	UICOMMON_LIBRARY_EXPORTED static void onMouseUp(UIBox& box, size_t button, const MouseState& state);
+	UICOMMON_LIBRARY_EXPORTED static void onMouseEnter(UIBox& box, const MouseState& state);
+	UICOMMON_LIBRARY_EXPORTED static void onMouseExit(UIBox& box, const MouseState& state);
+
+	UICOMMON_LIBRARY_EXPORTED static void draw(const UIBox& box, const Box2f& clipRectangle, const Box2f& targetRectangle, Canvas& target);
+
+private:
+	static inline UIBoxClass initClass();
+};
+
+UICOMMON_LIBRARY_NAMESPACE_END
+OUTER_NAMESPACE_END
